2847b024f12c27eaa81db267d4f1c78e8837c5f5
PLEXUS\adrian.perezmorales <aperezm111@iesalbarregas.es>
2026-03-25 09:49:19 +0100

    Estrucutra base proyecto final introduccion

--- Angular/primer-proyecto/src/app/pages/home/home.component.ts
+++ Angular/primer-proyecto/src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CardComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  autor = 'Adrián Pérez Morales';
 }
