354ee85f3d4a07663a111023a9dd18257d691efb
PLEXUS\adrian.perezmorales <aperezm111@iesalbarregas.es>
2026-03-26 09:43:26 +0100

    Hecha practica de observables y signals

--- Angular/primer-proyecto/src/app/pages/about/about.component.ts
+++ Angular/primer-proyecto/src/app/pages/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal, computed, effect } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
 import { UserService } from '../../services/user.service';
@@ -12,17 +12,44 @@ import { User } from '../../models/user';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent implements OnInit {
-  users: User[] = [];
   displayedColumns = ['id', 'username', 'email', 'phone'];
 
   private userService = inject(UserService);
   private router = inject(Router);
 
+  users = signal<User[]>([]);
+  filterText = signal('');
+  filteredUsers = computed(() =>
+    this.users().filter(user =>
+      user.username.toLowerCase().includes(this.filterText().toLowerCase()) &&
+      (this.selectedCity() === '' || user.address.city === this.selectedCity())
+    )
+  );
+  cities = computed(() =>
+    Array.from(new Set(this.users().map(user => user.address.city))).sort()
+  );
+  selectedCity = signal('');
+
   ngOnInit() {
     this.userService.getUsers().subscribe({
-      next: (data) => this.users = data,
+      next: (data) => this.users.set(data),
       error: (err) => console.error('Error al obtener usuarios:', err)
     });
+  }
+
+  onFilterChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.filterText.set(value);
+  }
+
+  onCityChange(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    this.selectedCity.set(value);
+  }
+
+  resetFilters() {
+    this.filterText.set('');
+    this.selectedCity.set('');
   }
 
   goToUserDetail(id: number): void {
