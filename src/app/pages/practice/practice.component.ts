2ba56dd57e0931f3f6232b1a13bc8a4141c62ab5
PLEXUS\adrian.perezmorales <aperezm111@iesalbarregas.es>
2026-04-09 09:40:01 +0200

    Hecha practica pipes de Angular

--- src/app/pages/practice/practice.component.ts
+++ src/app/pages/practice/practice.component.ts
@@ -6,6 +6,7 @@ import { FocusInputDirective } from '../../core/directives/focus-input.directive
 import { ShowIfRoleDirective } from '../../core/directives/show-if-role.directive';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { TranslatePipe } from '@ngx-translate/core';
+import { CurrencyPipe, DatePipe, JsonPipe, LowerCasePipe, PercentPipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-practice',
@@ -17,11 +18,33 @@ import { TranslatePipe } from '@ngx-translate/core';
     FocusInputDirective,
     ShowIfRoleDirective,
     FooterComponent,
-    TranslatePipe
+    TranslatePipe,
+    UpperCasePipe,
+    LowerCasePipe,
+    TitleCasePipe,
+    DatePipe,
+    CurrencyPipe,
+    PercentPipe,
+    SlicePipe,
+    JsonPipe
   ],
   templateUrl: './practice.component.html',
   styleUrl: './practice.component.scss'
 })
 export class PracticeComponent {
+  // Directives data
   selectedRole = 'admin';
+
+  // Pipes data
+  nombre = 'adrián pérez';
+  fechaActual = new Date();
+  precio = 1234.56;
+  progreso = 0.42;
+  texto = 'Angular es un framework de desarrollo web muy potente.';
+  objeto = {
+    nombre: 'Adrián',
+    edad: 25,
+    activo: true,
+    roles: ['admin', 'user']
+  };
 }
