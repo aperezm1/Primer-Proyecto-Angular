3fba813254aebb36957fc29947d4c93e5b9685ee
PLEXUS\adrian.perezmorales <aperezm111@iesalbarregas.es>
2026-04-09 13:56:44 +0200

    Hecho ej 9 gsharp

--- src/app/pages/practice/practice.component.ts
+++ src/app/pages/practice/practice.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HoverEnlargeDirective } from '../../core/directives/hover-enlarge.directive';
 import { CountClicksDirective } from '../../core/directives/count-clicks.directive';
@@ -6,11 +6,13 @@ import { FocusInputDirective } from '../../core/directives/focus-input.directive
 import { ShowIfRoleDirective } from '../../core/directives/show-if-role.directive';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { TranslatePipe } from '@ngx-translate/core';
-import { AsyncPipe, CurrencyPipe, DatePipe, JsonPipe, LowerCasePipe, PercentPipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import {AsyncPipe, CurrencyPipe, DatePipe, JsonPipe, LowerCasePipe, PercentPipe, SlicePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
 import { GreetPipe } from '../../core/pipes/greet.pipe';
 import { DoublePipe } from '../../core/pipes/double.pipe';
 import { BoldPipe } from '../../core/pipes/bold.pipe';
 import { AgePipe } from '../../core/pipes/age.pipe';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-practice',
@@ -40,11 +42,8 @@ import { AgePipe } from '../../core/pipes/age.pipe';
   templateUrl: './practice.component.html',
   styleUrl: './practice.component.scss'
 })
-export class PracticeComponent {
-  // Directives data
+export class PracticeComponent implements AfterViewInit, OnDestroy {
   selectedRole = 'admin';
-
-  // Pipes data
   name = 'adrián pérez';
   currentDate = new Date();
   price = 1234.56;
@@ -57,4 +56,31 @@ export class PracticeComponent {
   };
   number = 5;
   birthDate = new Date(2000, 6, 3);
+
+  ngAfterViewInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const sections = gsap.utils.toArray<HTMLElement>('.practice-page .section');
+
+    sections.forEach((section) => {
+      const cards = section.querySelectorAll('.card');
+
+      gsap.from(cards, {
+        y: 40,
+        opacity: 0,
+        duration: 0.7,
+        stagger: 0.12,
+        ease: 'power2.out',
+        scrollTrigger: {
+          trigger: section,
+          start: 'top 80%',
+          toggleActions: 'play none none reverse'
+        }
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+  }
 }
